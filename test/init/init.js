7d62e1af382e56ed01f26fea84a694fb33618d6b
Vipul Maheshwari <vipul@uohyd.ac.in>
2015-07-08 20:22:52 +0530

    jenkins

--- test/init/init.js
+++ test/init/init.js
@@ -1,8 +1,11 @@
 describe("Cloud App", function() {
     it("should init the CloudApp and SDK.", function(done) {
         this.timeout(500000);
-
         CB.CloudApp.init(CB.appId, CB.appKey);
-            done();
+        console.log("+++++++++++++++++++ Loaded SDK +++++++++++++++++++++");
+        console.log(CB);
+        console.log("++++++++++++++++++++Socket IO +++++++++++++++++++++++++++")
+        console.log(CB.io);
+        done();
     });
 });
--- test/join.js
+++ test/join.js
@@ -64,9 +64,12 @@ describe("Server Check",function(){
 describe("Cloud App", function() {
     it("should init the CloudApp and SDK.", function(done) {
         this.timeout(500000);
-
         CB.CloudApp.init(CB.appId, CB.appKey);
-            done();
+        console.log("+++++++++++++++++++ Loaded SDK +++++++++++++++++++++");
+        console.log(CB);
+        console.log("++++++++++++++++++++Socket IO +++++++++++++++++++++++++++")
+        console.log(CB.io);
+        done();
     });
 });
 
