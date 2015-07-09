ff432471ab9124b00a950d0fef02f00b1a9051e8
Vipul Maheshwari <vipul@uohyd.ac.in>
2015-07-09 10:20:35 +0530

    jenkins

--- test/init/init.js
+++ test/init/init.js
@@ -1,7 +1,12 @@
 describe("Cloud App", function() {
     it("should init the CloudApp and SDK.", function(done) {
         this.timeout(500000);
+
+        console.log('Invoked Init function');
+        console.log(CB);
+
         CB.CloudApp.init(CB.appId, CB.appKey);
+
         console.log("+++++++++++++++++++ Loaded SDK +++++++++++++++++++++");
         console.log(CB);
         console.log("++++++++++++++++++++ Socket IO +++++++++++++++++++++++++++")
--- test/join.js
+++ test/join.js
@@ -64,7 +64,12 @@ describe("Server Check",function(){
 describe("Cloud App", function() {
     it("should init the CloudApp and SDK.", function(done) {
         this.timeout(500000);
+
+        console.log('Invoked Init function');
+        console.log(CB);
+
         CB.CloudApp.init(CB.appId, CB.appKey);
+
         console.log("+++++++++++++++++++ Loaded SDK +++++++++++++++++++++");
         console.log(CB);
         console.log("++++++++++++++++++++ Socket IO +++++++++++++++++++++++++++")
