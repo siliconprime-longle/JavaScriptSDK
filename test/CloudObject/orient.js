c63730f262231e3e1db403570e20a235078e4d6c
Vipul Maheshwari <vipul@uohyd.ac.in>
2015-07-27 11:44:20 +0530

    orient

--- test/CloudObject/orient.js
+++ test/CloudObject/orient.js
@@ -1,6 +1,6 @@
 describe("Graph DB",function(done){
 
-    it("Should Create node",function(done){
+    /*it("Should Create node",function(done){
 
         this.timeout(10000);
 
@@ -16,7 +16,6 @@ describe("Graph DB",function(done){
             throw "unable to save the object";
         });
     });
-
     it("should save a relation",function(done){
 
         this.timeout(10000);
@@ -35,7 +34,7 @@ describe("Graph DB",function(done){
             console.log(err);
             throw "Unable to save data";
         })
-    });
+    });*/
 
     it("should save list of data",function(done){
 
@@ -57,6 +56,52 @@ describe("Graph DB",function(done){
             throw "Unable to save data";
         });
 
+    });
+
+   /* it("Should update a saved Record",function(done){
+
+        this.timeout(10000);
+        var obj = new CB.CloudObject('Employee');
+        obj.set('Name','Save');
+        obj.save().then(function(nobj){
+            nobj.set('Name','Update');
+            nobj.save().then(function(nnobj){
+                if(nnobj.get('Name') === 'Update')
+                    done();
+                else
+                    throw "Object saved but not updated";
+            },function(){
+               throw "unable to update the document";
+            });
+        },function(){
+            throw "Unable to save the record";
+        })
+
+    });
+
+    it("should update a relation",function(done){
+
+        this.timeout(10000);
+        var obj = new CB.CloudObject('Employee');
+        obj.set('Name','Anurag');
+        var obj1 = new CB.CloudObject('Company');
+        obj1.set('Name','CloudBoost');
+        obj.set('Company',obj1);
+        obj.save().then(function(nobj){
+            if(nobj.get('Company').get('Name') === 'CloudBoost') {
+                nobj.set('Company',null);
+                nobj.save().then(function(fobj){
+                    if(fobj.get('Company') === null)
+                        done();
+                },function(){
+                    throw "Unable to save object";
+                });
+            }else
+                throw "Unable to save relation";
+        },function(err){
+            console.log(err);
+            throw "Unable to save data";
+        })
     });
 
    /* it("should save list with relation",function(done){
