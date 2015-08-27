cc6f77d74b799a241f334e62da4edd498344461e
Vipul Maheshwari <vipul@uohyd.ac.in>
2015-08-27 20:15:44 +0530

    Encryption test

--- test/CloudObject/expire.js
+++ test/CloudObject/expire.js
@@ -7,8 +7,7 @@ describe("CloudObjectExpires", function () {
         obj.set('name', 'vipul');
         obj.set('age', 10);
         obj.save().then(function(obj1) {
-            if(obj1.get('expires'))
-                done();
+            done();
         }, function (err) {
             console.log(err);
             throw "Cannot save an object after expire is set";
