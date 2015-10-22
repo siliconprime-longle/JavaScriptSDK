b4ce0bb0d322ab3f1b70ba36dc3c326bc1688d0e
Nawaz Dhandala <nawazdhandala@outlook.com>
2015-10-22 09:42:39 -0300

    Added staging API.

--- test/init/init.js
+++ test/init/init.js
@@ -1,9 +1,11 @@
 describe("Cloud App", function() {
     it("should init the CloudApp and SDK.", function(done) {
         this.timeout(500000);
-
         CB.CloudApp.init(CB.appId, CB.appKey);
-
+        console.log("Service URL :"+CB.serviceUrl);
+        console.log("API URL :"+CB.apiUrl);	
+        console.log("App ID : "+CB.appId);
+        console.log("App Key : "+ CB.appKey);
         done();
     });
 });
