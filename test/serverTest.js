8fc3c90c61b46a55ef603af02fa0bceb123311c6
Nawaz Dhandala <nawazdhandala@outlook.com>
2015-12-24 19:40:26 -0300

    Tests integrated

--- test/serverTest.js
+++ test/serverTest.js
@@ -1,47 +1,13 @@
 describe("Server Check",function(){
     it("should check for localhost",function(done){
-    	this.timeout(10000);
-        var xmlhttp;
-        this.timeout(10000);
-        var req = typeof(require) === 'function' ? require : null;
-        // Load references to other dependencies
-        if (typeof(XMLHttpRequest) !== 'undefined') {
-             xmlhttp = XMLHttpRequest;
-            } else if (typeof(require) === 'function' &&
-                typeof(require.ensure) === 'undefined') {
-                xmlhttp = req('xmlhttprequest').XMLHttpRequest;
-            }
-            xmlhttp = new xmlhttp();
-        xmlhttp.open('GET','http://localhost:4730',true);
-        xmlhttp.send();
-        xmlhttp.onreadystatechange = function() {
-            if (xmlhttp.readyState == xmlhttp.DONE) {
-                if (xmlhttp.status == 200) {
-                    CB.appId = "sample123";
-                    CB.appKey = "9SPxp6D3OPWvxj0asw5ryA==";
-                    CB.masterKey = "Qopoy/kXd+6G734HsjQMqGPGOvwEJYmBG84lQawRmWM=";
-                    CB.jsKey = CB.appKey;
-                    CB.serverUrl = 'http://localhost:4730';
-                    CB.serviceUrl = 'http://localhost:3000';
-                    CB.socketIoUrl = CB.serverUrl;
-                    CB.apiUrl = CB.serverUrl;
-                    done();
-
-                }
-                else {
-                    
-                    console.log("Pinging staging");
-                    CB.serverUrl = 'http://stagingdataservices.azurewebsites.net';
-                    CB.serviceUrl = 'http://stagingfrontendservice.azurewebsites.net';
-                    CB.socketIoUrl = CB.serverUrl;
-                    CB.apiUrl = CB.serverUrl;
-                    CB.appId = 'stagingtest';
-                    CB.appKey = 'G3y7rAQJ9B+/TtliBknHsQ==';
-                    CB.jsKey = CB.appKey;
-                    CB.masterKey = "VvGQGjpKau/a4Z+XzB2E2o3Ih4D6CPWzKrK07TqntWg="
-                    done();
-                }
-            }
-        }
+        CB.appId = "sample123";
+        CB.appKey = "Yl90vim4+OP2y1XPUxIpyQ==";
+        CB.masterKey = "moHO7IQRlec3t5pbN6xet/1a9RCrlItqbGROyZHW7fw=";
+        CB.jsKey = CB.appKey;
+        CB.serverUrl = 'http://localhost:4730';
+        CB.serviceUrl = 'http://localhost:3000';
+        CB.socketIoUrl = CB.serverUrl;
+        CB.apiUrl = CB.serverUrl;
+        done();
     });
 });
