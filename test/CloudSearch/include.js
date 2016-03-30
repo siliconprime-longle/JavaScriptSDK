41eaa43eed9ea99fc48a319ea2ca0c6067bf42aa
rtbathula <battu.network@gmail.com>
2016-03-30 16:36:05 -0300

    sanitized include on search test

--- test/CloudSearch/include.js
+++ test/CloudSearch/include.js
@@ -18,31 +18,40 @@ describe("Inlcude in CloudSearch", function (done) {
                 cs.searchFilter = new CB.SearchFilter();
                 cs.searchFilter.include('newColumn7');
                 cs.searchFilter.equalTo('id',obj.id);
-                cs.search().then(function(  list){
-                    done();
-                    console.log(list);
+                
+                cs.search().then(function(  list){                  
                     if(list.length>0){
+
+                        var isSuccessfullJoin=true;
+
                         for(var i=0;i<list.length;i++){
-                            console.log('LIST');
-                            console.log(list[0]);
+
                             var student_obj=list[i].get('newColumn7');
-                            console.log('Student');
-                            console.log(student_obj);
+
                             if(Object.keys(student_obj.document).length >3) {
-                                if (!student_obj.get('name'))
-                                    throw "Unsuccessful Join";
-                            } else
-                                done();
+                                if (!student_obj.get('name')){
+                                    isSuccessfullJoin=false;
+                                    break;
+                                }
+                            } else{
+                                isSuccessfullJoin=true;
+                            }
                         }
+
+                        if(isSuccessfullJoin){
+                            done();
+                        }else{
+                            done("Unsuccessful Join");
+                        }
+
                     }else{
-                        throw "Cannot retrieve a saved relation.";
+                        done("Cannot retrieve a saved relation.");
                     }
                 }, function(error){
-                    throw "Unsuccessful join"
+                    done(error);                    
                 });
             }, error : function(error){
-                throw "Cannot save a CloudObject";
-
+                done(error);
             }
 
         });
