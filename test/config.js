120c20b4a900431d690141677c5ddfb826b2320e
shubham <shubhamqweasd@gmail.com>
2016-12-13 14:46:00 +0530

    fix config

--- test/config.js
+++ test/config.js
@@ -1,2 +1,5 @@
-var SECURE_KEY = "e25b1b0d-06b2-4d9f-aaa7-b8c250bd653f";
+// var SECURE_KEY = "e25b1b0d-06b2-4d9f-aaa7-b8c250bd653f";
+// var URL = "http://localhost:4730";
+
+var SECURE_KEY = "1227d1c4-1385-4d5f-ae73-23e99f74b006";
 var URL = "http://localhost:4730";
