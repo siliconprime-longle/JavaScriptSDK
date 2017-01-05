c789948bd7aa8827e784f2e05640863659e9bdf4
Utkarsh Gupta <2012uec1641@mnit.ac.in>
2017-01-05 14:09:14 +0530

    Added defaultValue property to CB.Column class in JavaScriptSDK. (#28)
    * Added a default value
    
    * Done setting up the default value
    
    * corrected the glitch in the code

--- src/Column.js
+++ src/Column.js
@@ -2,30 +2,30 @@ import CB from './CB'
 /*
  Column.js
  */
-class Column{
+class Column {
   constructor(columnName, dataType, required, unique){
      this.document = {};
      if(columnName){
        CB._columnNameValidation(columnName);
        this.document.name = columnName;
        this.document._type = 'column';
-     }
+     } 
 
-     if(dataType){
+     if(dataType) {
        CB._columnDataTypeValidation(dataType);
        this.document.dataType = dataType;
-     }else{
+     } else {
        this.document.dataType = "Text";
      }
 
-     if(typeof(required) === 'boolean'){
+     if(typeof(required) === 'boolean') {
        this.document.required = required;
      }
-     else{
+     else {
        this.document.required = false;
      }
 
-     if(typeof(unique) === 'boolean'){
+     if(typeof(unique) === 'boolean') {
        this.document.unique = unique;
      }
      else{
@@ -43,6 +43,7 @@ class Column{
      this.document.isEditable = true;
      this.document.isRenamable = false;
      this.document.editableByMasterKey = false; 
+     this.document.defaultValue = null;
   };
 }
 
@@ -90,6 +91,15 @@ Object.defineProperty(Column.prototype,'required',{
     },
     set: function(required) {
         this.document.required = required;
+    }
+});
+
+Object.defineProperty(Column.prototype,'defaultValue',{
+    get: function() {
+        return this.document.defaultValue;
+    },
+    set: function(defaultValue) {
+        this.document.defaultValue = defaultValue;
     }
 });
 
