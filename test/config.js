0bf97d02b06da6630d80fe02540cfd0dbeb7b432
Utkarsh Gupta <utkarshindia@live.in>
2017-01-11 10:22:56 +0530

    Uncommented all the tests to check

--- test/config.js
+++ test/config.js
@@ -1,5 +1,5 @@
-// var SECURE_KEY = "51d3ce3e-50fd-406e-9c5d-c6d89556887c";
+var SECURE_KEY = "47dfc8b3-7c7a-4661-8e71-36ed0aaa0563";
 // var URL = "http://localhost:4730";
 
-var SECURE_KEY = "47dfc8b3-7c7a-4661-8e71-36ed0aaa0563";
+//var SECURE_KEY = "1227d1c4-1385-4d5f-ae73-23e99f74b006";
 var URL = "http://localhost:4730";
