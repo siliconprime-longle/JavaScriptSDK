381cfccf4e5c7ac6566512c610bcca750e63e6c0
Ritish Gumber <ritishgumber@gmail.com>
2017-02-08 13:48:26 +0530

    key changed

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 //var SECURE_KEY = "47dfc8b3-7c7a-4661-8e71-36ed0aaa0563";
-var SECURE_KEY = "84482db3-edfb-4fce-8ed6-9ce8130b74bd";
+var SECURE_KEY = "1227d1c4-1385-4d5f-ae73-23e99f74b006";
 
 var URL = "http://localhost:4730";
 
