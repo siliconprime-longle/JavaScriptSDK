6106d7a556dd9167ae0e12e13f04a95d09a83280
kashishchakarvarti <kashpsyranger@gmail.com>
2017-03-03 11:10:36 +0530

    removed comments

--- test/misc/export.js
+++ test/misc/export.js
@@ -7,23 +7,6 @@ describe("Export Table",function(done){
         this.timeout(10000);
         CB.appKey = CB.masterKey;
     });
-
-    function convertToJSON(data){
-        var def = q.defer()
-        arr =[]; 
-
-          csv()
-       .fromString(data)
-       .on('json', (json) => {
-           arr.push(json)
-       })
-       .on('done',(error)=>{
-           if(error) def.reject(error)
-           def.resolve(arr)
-       })
-
-       return def.promise  
-    }
     var savedObject = [];
     it("should create a table",function(done){
 
@@ -69,52 +52,7 @@ describe("Export Table",function(done){
             appKey:CB.appKey
         }).then(function(data){
             if(data.data){
-                
-               // convertToJSON(data.data).then(function(csvObject){
-               
-               // csvObject = csvObject[0]
-               // savedObject= savedObject[0].document;
-
-               // var csvObjectKeys = Object.keys(csvObject);
-               // var savedObjectKeys = Object.keys(savedObject)
-               // var objectsEqual = true;
-               // if(savedObjectKeys.length !== csvObjectKeys.length)
-               // {
-               //   objectsEqual = false;
-               // }
-               // if(null=='')
-               // {
-               // console.log('f')
-               // }
-
-               // console.log(savedObject)
-               // console.log('================================================')
-               // console.log(csvObject)
-               //console.log(csvObject[csvObjectKeys[0]])
-               // for(let j =0;j<csvObjectKeys.length;j++)
-               // { var csvKey = csvObjectKeys[j];
-               //   var savedKey = savedObjectKeys[j];
-
-
-               //   // if(csvObject[csvKey] !== savedObject[savedKey])
-               //   // {
-               //   //    objectsEqual = false;
-               //   // }
-               //   console.log(savedObject[savedKey])
-                
-               // }
-              //for()
-                
-              // if( JSON.stringify(csvObject) === JSON.stringify(savedObject) )
-              //   {console.log(csvObject) ;
-              //       done()
-              //   }
                done();
-            //}
-                
-                // },function(error){
-                //     done('ERROR')
-                // })
             } else {
                 done('ERROR')
             }
